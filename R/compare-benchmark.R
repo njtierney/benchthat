7d83f17fe8d0f5f159a4d84e8da1238813189099
njtierney <nicholas.tierney@gmail.com>
2025-06-07 15:11:03 +1000

    document

--- R/compare-benchmark.R
+++ R/compare-benchmark.R
@@ -1,3 +1,25 @@
+#' Combine two benchmarks into one
+#'
+#' @param current current benchmark
+#' @param new new benchmark
+#'
+#' @returns benchmark object with names "current" and "new" appended to expression.
+#' @export
+#'
+#' @examples
+#' dat <- data.frame(x = runif(100, 1, 1000), y = runif(10, 1, 1000))
+#' bm1 <- bench::mark(
+#'     subset = dat[dat$x > 500, ]
+#' )
+#'
+#' bm2 <- bench::mark(
+#'     subset = dat[which(dat$x > 500), ]
+#' )
+#'
+#' combined_bm <- bench_combine(
+#'     bm1,
+#'     bm2
+#' )
 bench_combine <- function(current, new) {
   list(
     current = current,
